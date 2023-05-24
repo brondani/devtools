f6843509c8cf4aec10122cefd954dc785c2d5e1a
Daniel Brondani <daniel.brondani@arm.com>
2023-05-24 12:34:30 +0200

    Update create_installer.sh

--- tools/buildmgr/cbuildgen/installer/create_installer.sh
+++ tools/buildmgr/cbuildgen/installer/create_installer.sh
@@ -71,7 +71,7 @@ curl --retry 3 -L ${cpackget_base}_windows_arm64.zip   -o temp.zip && unzip -p t
 curl --retry 3 -L ${cpackget_base}_darwin_arm64.tar.gz -o - | tar xzfO - --wildcards    '*cpackget'     > ${distdir}/bin/cpackget.mac-arm64
 
 # Get csolution
-csolution_version="1.7.0"
+csolution_version="2.0.0-dev0"
 csolution_base=https://github.com/Open-CMSIS-Pack/devtools/releases/download/tools%2Fprojmgr%2F${csolution_version}/projmgr.zip
 curl --retry 3 -L ${csolution_base} -o temp.zip && unzip -q -d temp temp.zip
 cp 'temp/bin/linux-amd64/csolution' ${distdir}/bin/csolution.lin-amd64
